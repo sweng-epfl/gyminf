bce69a9c758401302a503c5a0128a04a0f056c0d
Solal Pirelli <solal.pirelli@gmail.com>
2022-05-03 17:16:13 +0200

    Ajout d'un squelette de tests pour tests/cours/app

--- tests/cours/app.py
+++ tests/cours/app.py
@@ -2,10 +2,10 @@ from joker import Joker
 
 # Méthode principale séparée afin de pouvoir la tester
 def main():
-    print("Bonjour ! Combien de blagues voulez-vous ? (en anglais, désolé, pas de bonne API pour blagues française...)", end=" ")
+    print("Bonjour ! Combien de blagues voulez-vous ?")
     count = int(input())
     Joker().get_jokes(count)
-    print("J'espère que vous avez bien ri ! Au revoir !")
+    print("J'espère que c'était drôle !")
 
 if __name__ == "__main__":
     main()
---
+++ tests/cours/app_test.py
@@ -0,0 +1,23 @@
+from unittest import TestCase
+from hamcrest import *
+
+# permet de remplacer "input" par ce que l'on veut
+# (consultez la doc si vous souhaitez en savoir plus)
+from unittest.mock import patch
+
+from app import main
+
+class AppTests(TestCase):
+    def test_1_joke(self):
+        # on "mock" des fonctions/objets avec patch
+        # "patch" avec 1 seul argument ajoute un argument à la fonction qui représente le "mock"
+        # avec 2 arguments, il permet de rapidement patcher une fonction ou un objet sans explicitement représenter le "mock"
+        @patch("builtins.print")
+        @patch("builtins.input", lambda *args: '1')
+        def core(mock_print):
+            main()
+            # TODO : tester plus que juste le nombre d'appels :-)
+            assert_that(mock_print.call_args_list, has_length(3))
+        core() # sans argument, ils sera fournis par le décorateur "patch"... c'est un peu magique mais ça marche
+
+    # TODO : que se passe-t-il si on demande 0 blagues ? -10 ? un lézard ?
