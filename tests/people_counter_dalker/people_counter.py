196cdc4aea15994e08bafd640ec69564d2edb2c4
Daniel Kessler (a.k.a. Dalker) <daniel.kessler@dalker.org>
2022-05-07 15:29:32 +0200

    Résolu exercice People Counter (Dalker)

---
+++ tests/people_counter_dalker/people_counter.py
@@ -0,0 +1,10 @@
+class PeopleCounter:
+
+    def __init__(self):
+        self.count = 0
+
+    def increment(self):
+        self.count += 1
+
+    def reset(self):
+        self.count = 0
---
+++ tests/people_counter_dalker/test_people_counter.py
@@ -0,0 +1,28 @@
+import unittest
+from hamcrest import assert_that, equal_to
+from people_counter import PeopleCounter
+
+
+class TestPeopleCounter(unittest.TestCase):
+    def test_init(self):
+        counter = PeopleCounter()
+        assert_that(counter.count, equal_to(0))
+
+    def test_increment_one(self):
+        counter = PeopleCounter()
+        counter.increment()
+        assert_that(counter.count, equal_to(1))
+
+    def test_increment_more(self):
+        counter = PeopleCounter()
+        counter.increment()
+        counter.increment()
+        assert_that(counter.count, equal_to(2))
+        counter.increment()
+        assert_that(counter.count, equal_to(3))
+
+    def test_reset(self):
+        counter = PeopleCounter()
+        counter.increment()
+        counter.reset()
+        assert_that(counter.count, equal_to(0))
