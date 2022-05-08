059086bcc43af3c134870a267bd1a503e3c64f96
Solal Pirelli <solal.pirelli@gmail.com>
2022-05-08 16:20:09 +0200

    Ajout de la Q5 pour tests

---
+++ tests/Q5/ReadMe.md
@@ -0,0 +1,3 @@
+Votre tâche est de développer une classe de vecteur 3D, `Vector3`, en fonction de la spécification donnée dans le fichier `vector3.py`.
+Essayez de le faire avec du "test-driven development", soit en écrivant tous les tests pour la classe entière d'abord, soit fonction par fonction.
+Réfléchissez bien aux cas limites.
---
+++ tests/Q5/vector3.py
@@ -0,0 +1,9 @@
+# Vecteur en 3 dimensions avec composants entiers.
+# Cette classe doit pouvoir :
+# - vérifier si un vecteur est le vecteur nul
+# - calculer la longueur d'un vecteur
+# - calculer le produit scalaire de deux vecteurs
+# - calculer le produit vectoriel de deux vecteurs
+# Cette classe doit être robuste en ce qui concerne les mauvaises entrées, p.ex., "None" comme dimension
+class Vector3:
+    pass
