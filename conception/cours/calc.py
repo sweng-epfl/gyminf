24e9212f5711c639d743c8d5a24964d81a43df35
Solal Pirelli <solal.pirelli@gmail.com>
2022-05-15 14:57:05 +0200

    Ajout du premier exercice de conception en cours

---
+++ conception/cours/calc.py
@@ -0,0 +1,32 @@
+while True:
+    text = input("Calcul à faire ? (en notation polonaise inverse; ou 'sortir') ")
+    if text == "sortir": break
+    parts = text.split(" ")
+    lst = []
+    for p in parts:
+        if p == '':
+            continue
+        elif p == '+':
+            lst.append(lst.pop() + lst.pop())
+        elif p == '-':
+            r = lst.pop()
+            l = lst.pop()
+            lst.append(l - r)
+        elif p == '*':
+            lst.append(lst.pop() * lst.pop())
+        elif p == '/':
+            r = lst.pop()
+            l = lst.pop()
+            lst.append(l / r)
+        else:
+            try:
+                n = int(p)
+                lst.append(n)
+            except ValueError:
+                print("Erreur, je ne sais pas quoi faire avec : " + p)
+                continue
+
+    if len(lst) == 1:
+        print(lst.pop())
+    else:
+        print("Calcul invalide")
