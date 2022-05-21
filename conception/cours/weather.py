52ee6bc2cf8bdc26946e0430f400a4f614a726c1
Georges <ga@gando.ch>
2022-05-21 12:56:46 +0200

    Solution de weather.py

--- conception/cours/weather.py
+++ conception/cours/weather.py
@@ -1,14 +1,44 @@
-# Exercice : Convertissez cette application pour utiliser le pattern MVP, donc :
-#            un "model" qui contient la logique, une "view" dédiée à l'affichage et aux interactions utilisateur, et un "presenter" qui médie
+# Exercice : Convertissez cette application pour utiliser le pattern MVP
+# donc : un "model" qui contient la logique, une "view" dédiée à l'affichage
+# et aux interactions utilisateur, et un "presenter" qui médie
 
 import random
 
-while True:
-    input("Appuyez sur Entrée pour vérifier la météo")
-    weather = random.randrange(0, 4)
-    if weather == 0:
-        print("Météo actuelle : Ensoleillé")
-    elif weather == 1:
-        print("Météo actuelle : Pluvieux")
-    else:
-        print("Météo actuelle : ???")
+
+class Modele():
+
+    def getMeteo(self):
+        meteo = random.randrange(0, 4)
+        if meteo == 0:
+            return "Ensoleillé"
+        elif meteo == 1:
+            return "Pluvieux"
+        else:
+            return "???"
+
+
+class Vue():
+
+    def affiche(self, meteo):
+        print("Météo actuelle : " + meteo)
+
+    def attend_entree(self, presenter):
+        input("Appuyez sur Entrée pour vérifier la météo ")
+        presenter.rend_entree()
+
+
+class Presenteur():
+
+    def __init__(self):
+        self.modele = Modele()
+        self.vue = Vue()
+        self.run()
+
+    def run(self):
+        while True:
+            self.vue.attend_entree(self)
+
+    def rend_entree(self):
+        self.vue.affiche(self.modele.getMeteo())
+
+Presenteur()
